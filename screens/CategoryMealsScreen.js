c36f22d72f323f7758d8f3ebbdb2fdfa2b06e1a7
Neimerc <Neil_Mercury@yahoo.es>
2021-03-27 05:11:55 +0100

    Navigation to the MealDetailsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export const CategoryMealsScreen = () => {
+export const CategoryMealsScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen!</Text>
+      <Button
+        title="Go to Meal Details!"
+        onPress={() => {
+          navigation.navigate('MealDetail');
+        }}
+      />
     </View>
   );
 };
