421774e521454e34cde86ff36bb8de869791f8ae
Neimerc <Neil_Mercury@yahoo.es>
2021-03-27 05:42:07 +0100

    Outputting a Grid of Categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 export const CategoriesScreen = ({ navigation }) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go to Meals!"
-        onPress={() => {
-          // navigation.navigate('CategoryMeals')
-          navigation.navigate({ routeName: 'CategoryMeals' });
-
-          // push works like navigate.
-          //navigation.push('CategoryMeals');
-          // But you can go to a page where you already are on
-          //navigation.push('Categories');
-
-          // To go to CategoryMeals but you don't want to add it to the stack, but
-          // you want to instead replace your current component in the stack with it.
-          // That means that you won't be able to go back on the new page, because
-          // the stack would be empty (in this example) thereafter
-          // This could be used on a login screen where a user did sign in, and once
-          // signed in, the user shouldn't be able to go back to the login screen.
-          //navigation.replace('CategoryMeals');
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={item => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
@@ -34,5 +27,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
