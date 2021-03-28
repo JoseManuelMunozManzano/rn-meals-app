fe56b7d19224d68fffba0d10284544efbcd941df
Neimerc <Neil_Mercury@yahoo.es>
2021-03-28 10:39:33 +0200

    Setting Icons and Configuring Tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import { CategoriesScreen } from '../screens/CategoriesScreen';
 import { CategoryMealsScreen } from '../screens/CategoryMealsScreen';
@@ -21,6 +23,8 @@ const MealsNavigator = createStackNavigator(
     MealDetail: MealDetailScreen,
   },
   {
+    // navigationOptions in the config of a navigator only matter if that navigator is used inside of
+    // another navigator
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
@@ -34,11 +38,41 @@ const MealsNavigator = createStackNavigator(
 );
 
 // We can (and must!) use multiple navigators.
-const MealsFavTabNavigator = createBottomTabNavigator({
-  // One is our Stack Navigator
-  Meals: MealsNavigator,
-  Favorites: FavoritesScreen,
-});
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    // One is our Stack Navigator (We can (and must!) nested navigators)
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: 'Favorites!',
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
 
 // We export MealsFavTabNavigator that includes MealsNavigator
 export default createAppContainer(MealsFavTabNavigator);
