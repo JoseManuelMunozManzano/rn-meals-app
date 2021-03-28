d459fe11fb5f598af0efd0511c949cec56d6e0e4
Neimerc <Neil_Mercury@yahoo.es>
2021-03-28 10:25:56 +0200

    Adding Tabs-based Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import { CategoriesScreen } from '../screens/CategoriesScreen';
 import { CategoryMealsScreen } from '../screens/CategoryMealsScreen';
 import { MealDetailScreen } from '../screens/MealDetailScreen';
+import { FavoritesScreen } from '../screens/FavoritesScreen';
 
 import Colors from '../constants/Color';
 
@@ -12,17 +14,13 @@ const MealsNavigator = createStackNavigator(
   {
     Categories: {
       screen: CategoriesScreen,
-      // First point when you can declare navigationOptions
-      // navigationOptions: { headerTitle: 'Meal Categories Win' },
     },
     CategoryMeals: {
       screen: CategoryMealsScreen,
     },
     MealDetail: MealDetailScreen,
   },
-  // Second point when you can declare navigationOptions
   {
-    // defaultNavigationOptions allows you to set up options that apply to every screen
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
@@ -31,14 +29,16 @@ const MealsNavigator = createStackNavigator(
         Platform.OS === 'android' ? 'white' : Colors.primaryColor,
     },
 
-    // mode. The default value is card
-    // It's the type of transition between screens. Only for iOS
     mode: 'modal',
-
-    // The default initial Route is the first of the above (Categories)
-    // but you can change it
-    // initialRouteName: 'MealDetail',
   }
 );
 
-export default createAppContainer(MealsNavigator);
+// We can (and must!) use multiple navigators.
+const MealsFavTabNavigator = createBottomTabNavigator({
+  // One is our Stack Navigator
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen,
+});
+
+// We export MealsFavTabNavigator that includes MealsNavigator
+export default createAppContainer(MealsFavTabNavigator);
